e30da11649997f15cb2ea0f7ecf8f35d56c97a10
Sruthin Gaddam <sruthingaddam@Sruthins-MacBook-Air.local>
2016-08-02 14:33:33 -0400

    initial Commit

---
+++ GhostGossip/UserModel.swift
@@ -0,0 +1,13 @@
+//
+//  UserModel.swift
+//  GhostGossip
+//
+//  Created by Sruthin Gaddam on 8/2/16.
+//  Copyright © 2016 Sruthin Gaddam. All rights reserved.
+//
+
+import UIKit
+
+class UserModel: NSObject {
+
+}
